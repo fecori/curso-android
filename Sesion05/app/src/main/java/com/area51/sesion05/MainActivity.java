bbd1fb90436291c7e4d6403c26205eb7fb587cf3
Joel Porras <jporras24@gmail.com>
2015-02-04 09:33:03 -0500

    Agregando clase del dia

---
+++ Sesion05/app/src/main/java/com/area51/sesion05/MainActivity.java
@@ -0,0 +1,48 @@
+package com.area51.sesion05;
+
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView lblWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lblWelcome=(TextView)findViewById(R.id.lblWelcome);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/A.C.M.E. Secret Agent.ttf");
+        lblWelcome.setTypeface(typeface);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
