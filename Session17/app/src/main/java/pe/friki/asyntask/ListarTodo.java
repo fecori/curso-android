1cb4eb1aeece9a1635c90cc990795b4860162e39
Francisco Cordova Rios <fecori@gmail.com>
2015-03-02 08:36:36 -0500

    Agregando clase del dia

---
+++ Session17/app/src/main/java/pe/friki/asyntask/ListarTodo.java
@@ -0,0 +1,46 @@
+package pe.friki.asyntask;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import pe.friki.libs.RESTClient;
+import pe.friki.session17.MainActivity;
+
+/**
+ * Created by ios2015 on 3/2/15.
+ */
+public class ListarTodo extends AsyncTask<String, Void, String> {
+
+    Context context;
+    ProgressDialog progressDialog;
+
+    public ListarTodo(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Cargando datos...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String resultado = "";
+        for (String url : params) {
+            resultado = RESTClient.connectAndReturnResponse(url);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        progressDialog.dismiss();
+        ((MainActivity) context).resultadoListarTodo(s);
+    }
+}
---
+++ Session17/app/src/main/java/pe/friki/asyntask/ManageAsyntask.java
@@ -0,0 +1,18 @@
+package pe.friki.asyntask;
+
+import android.content.Context;
+
+/**
+ * Created by ios2015 on 3/2/15.
+ */
+public class ManageAsyntask {
+
+    Context context;
+
+    public ManageAsyntask(Context context) {
+        this.context = context;
+    }
+
+
+
+}
